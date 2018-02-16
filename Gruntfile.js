efa05552212587228f0a896e889a32f41c712272
Subhod I <subhodpoojary@gmail.com>
2018-02-16 18:41:38 +0530

    Add uglify task to grunt |  travis code coverage  run post script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,35 @@
 'use strict';
-
 module.exports = function (grunt) {
+
     grunt.initConfig({
-        coveralls: {
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
             options: {
-                force: false
+                separator: ';'
             },
-            your_target: {
-                src: 'coverage/*.info',
-                options: {
-                }
-            },
+            dist: {
+                src: ['dist/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
         },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'qunit']
+        }
     });
-    grunt.loadNpmTasks('grunt-coveralls');
-}
+
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
